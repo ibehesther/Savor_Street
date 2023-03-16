aaf45be8e62db24233b8af7d36119791d9deaf4a
Ibeh Esther <estheribeh5@gmail.com>
2023-03-17 00:33:42 +0100

    refactor: Change Date string formatting

--- src/utils/generate_mssg.ts
+++ src/utils/generate_mssg.ts
@@ -1,9 +1,14 @@
 import moment from 'moment';
 import { MessageDTO } from 'src/dto/message.dto';
+
+const date = new Date(Date.now());
+let createdAt=date.toString().split(" ")[4];
+createdAt = createdAt.split("").splice(0, 5).join('');
+
 export const generateMessage = (user: string, obj: MessageDTO) => {
     return {
         user,
         obj,
-        createdAt: moment(new Date().getTime()).format("HH:mm")
+        createdAt
     }
 }
