c8ff31d5cdd21e08f48b8744508b584dffad32bc
Ibeh Esther <estheribeh5@gmail.com>
2023-03-17 01:08:35 +0100

    refactor: Change date format to ISOString

--- src/utils/generate_mssg.ts
+++ src/utils/generate_mssg.ts
@@ -1,14 +1,10 @@
 import moment from 'moment';
 import { MessageDTO } from 'src/dto/message.dto';
 
-const date = new Date(Date.now());
-let createdAt=date.toString().split(" ")[4];
-createdAt = createdAt.split("").splice(0, 5).join('');
-
 export const generateMessage = (user: string, obj: MessageDTO) => {
     return {
         user,
         obj,
-        createdAt
+        createdAt: moment(new Date().toISOString()).format("HH:mm")
     }
 }
